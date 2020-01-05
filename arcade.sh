13e285af7a5a5a51a8142c7012cb026f9452aab8
makotoworkshop <makoto@debianbox.makotoworkshop.org>
2020-01-05 12:11:24 +0100

    Update arcade.sh

--- arcade.sh
+++ arcade.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo modprobe fbtft_device custom name=fb_ili9341 gpios=reset:24,dc:25,led:18 speed=64000000 fps=60 bgr=1 rotate=270
+#sudo modprobe fbtft_device custom name=fb_ili9341 gpios=reset:24,dc:25,led:18 speed=64000000 fps=60 bgr=1 rotate=270
 #sudo SDL_VIDEODRIVER=fbcon SDL_FBDEV=/dev/fb1 mplayer -vo sdl -framedrop /home/pi/deathsml_15-07-2015_1cc_Xvid.avi
 
-sleep 4
+#sleep 4
 /usr/local/bin/fbcp &
 
 #mplayer 01-Bridge\ Burning.mp3
